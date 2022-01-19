04b3fda85c0739c07b4c8f71faac2a696cb711f1
minsoo cho <khg04170@gmail.com>
2022-01-19 14:16:48 +0000

    airbnb completed, without image uploading

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,46 @@
 class FlatsController < ApplicationController
+  before_action :find_flat, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_param)
+      if @flat.save
+        redirect_to flats_path
+      else
+        render :new
+      end
+  end
+
+  def show; end
+
+  def edit
+  end
+
+  def update
+    @flat.update(flat_param)
+    redirect_to flats_path
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_param
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+
 end
